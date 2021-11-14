454b2cd89c5beb83677627e25225aecdca5c55b6
CarloSilva96 <carlosilva.dev@gmail.com>
2021-11-14 13:34:19 -0300

    feito servidor back end e adicionado no docker-compose

---
+++ app/sender.py
@@ -0,0 +1,16 @@
+from bottle import Bottle, route, run, request
+
+#ROTA DE ENVIO POST
+@route('/', method='POST')
+def send():
+    assunto = request.forms.get('assunto')
+    mensagem = request.forms.get('mensagem')
+    print (a)
+    return 'Enviado! \n Assunto: {assunto}, Mensagem: {mensagem}'.format(
+        assunto = assunto,
+        mensagem = mensagem
+    )
+    
+#VERIFICANDO SE É O ARQUIVO PRINCIPAL
+if __name__ == '__main__':
+    run(host = '0.0.0.0', port = 8080, debug = True)
